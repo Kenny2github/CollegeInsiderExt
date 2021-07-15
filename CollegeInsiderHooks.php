ac623623ab5e523e950891c09bb627c2ea6ed884
Kenny2github <kenny2minecraft@gmail.com>
2021-07-15 21:42:06 +0000

    Implement {{#article:}}
    * Order of arguments is now
      0. Index of article (0-indexed, by date)
      1. Type of article (one of COLLEGEINSIDER_TYPES, defaults to all)
      2. CSS class on wrapper div (default "news")
    * Joins collegeinsider and categorylinks to limit by category
    * Finds the file and uses it
    * Basic error message if no article is found by conditions

--- CollegeInsiderHooks.php
+++ CollegeInsiderHooks.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/defines.php';
 
 class CollegeInsiderHooks {
 	public static function onParserFirstCallInit( Parser $parser ) {
@@ -6,9 +7,53 @@ class CollegeInsiderHooks {
 	}
 
 	public static function renderArticle( Parser $parser, PPFrame $frame, $args ) {
-		$type = $frame->expand($args[0]);
-		$index = intval($frame->expand($args[1]));
-		return "(Would get the $index-th $type here)"; // TODO
+		$dbr = wfGetDB( DB_REPLICA );
+		$index = intval( $frame->expand( $args[0] ) );
+		$type = isset( $args[1] ) ? $frame->expand( $args[1] ) : '*';
+		$cls = isset( $args[2] ) ? $frame->expand( $args[2] ) : 'news';
+		if ( !$type || $type == '*' ) {
+			$conds = [];
+		} else {
+			$conds = [ 'cl_to' => str_replace( ' ', '_', wfMessage(
+				"collegeinsider-type-$type"
+			)->text() ) . 's' ];
+		}
+		$row = $dbr->selectRow(
+			[ 'ci' => 'collegeinsider', 'cl' => 'categorylinks' ],
+			[ 'page_id', 'thumbnail', 'blurb' ],
+			$conds, __METHOD__,
+			[ 'ORDER BY' => 'datestamp DESC', 'OFFSET' => $index ],
+			[ 'cl' => [ 'INNER JOIN', 'page_id=cl_from' ] ]
+		);
+		if ( !$row ) return wfMessage(
+			'collegeinsider-invalid-article',
+			$index, $type
+		)->text();
+		$file = wfFindFile( $row->thumbnail );
+		if ( !$file ) return $row->thumbnail;
+		$file = $file->getUrl();
+		$description = htmlspecialchars( $row->blurb );
+		$page = WikiPage::newFromID( $row->page_id );
+		$pageTitle = $page->getTitle();
+		$h3 = $pageTitle->getText();
+		$link = $pageTitle->getLocalURL();
+		$hashtags = [];
+		foreach ( $page->getCategories() as $title ) {
+			$key = COLLEGEINSIDER_CATEGORIES[$title->getText()];
+			if ( !$key ) continue;
+			$url = $title->getLocalURL();
+			$hashtags[] = "<a href=\"$url\">#positive$key</a>";
+		}
+		$hashtags = implode( '', $hashtags ) ?: '<a>#positiveeducation</a>';
+		return [ <<<EOS
+<div class="$cls" style="background-image: url('$file');">
+	<div class="hashtags">&nbsp;$hashtags</div>
+	<a href="$link"><div class="description">
+		<h3>$h3</h3>
+		<p class="long-description">$description</p>
+	</div></a>
+</div>
+EOS, 'noparse' => true, 'isHTML' => true ];
 	}
 
 	public static function onLoadExtensionSchemaUpdates( $updater ) {
